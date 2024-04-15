3128c88bd468105498f48616c2cc330a9f1bf0e1
vanmuoi24 <domuoigghh@gmail.com>
2024-04-15 10:39:10 +0700

    muoi

--- Form/MVC/Modal/RegisterModal.php
+++ Form/MVC/Modal/RegisterModal.php
@@ -15,7 +15,7 @@ class RegisterModal
         $sdt = $data["phoneValue"];
         $email = $data["emailValue"];
         $diachi = $data["addressValue"];
-        $tendn = $data["usernameValue"];
+        $tendn = $data["usernameValue"]; // Mã khách hàng nhập từ người dùng
         $matkhau = $data["passwordValue"];
         $xacnhanmk = $data["confirmPasswordValue"];
 
@@ -27,10 +27,22 @@ class RegisterModal
             return "Xác nhận mật khẩu không khớp";
         }
 
+        // Kiểm tra tính duy nhất của TenDN
+        $sql_check_tendn = "SELECT COUNT(*) AS total FROM taikhoan WHERE TenDN = ?";
+        $stmt_check_tendn = $this->conn->prepare($sql_check_tendn);
+        $stmt_check_tendn->bind_param('s', $tendn);
+        $stmt_check_tendn->execute();
+        $result_check_tendn = $stmt_check_tendn->get_result();
+        $row_check_tendn = $result_check_tendn->fetch_assoc();
+
+        if ($row_check_tendn['total'] > 0) {
+            return "Tên đăng nhập đã tồn tại";
+        }
+
         // Thêm thông tin khách hàng vào bảng khachhang
-        $sql_khachhang = "INSERT INTO khachhang (Ho, Ten, DiaChi, Email,MaKH) VALUES (?, ?, ?, ?,?)";
+        $sql_khachhang = "INSERT INTO khachhang (MaKH, Ho, Ten, DiaChi, Email) VALUES (?, ?, ?, ?, ?)";
         $stmt_khachhang = $this->conn->prepare($sql_khachhang);
-        $stmt_khachhang->bind_param('sssss', $ho, $ten, $diachi, $email, $tendn);
+        $stmt_khachhang->bind_param('sssss', $tendn, $ho, $ten, $diachi, $email);
         $stmt_khachhang->execute();
 
         if ($stmt_khachhang->error) {
@@ -38,7 +50,7 @@ class RegisterModal
         }
 
         // Thêm thông tin tài khoản vào bảng taikhoan
-        $sql_taikhoan = "INSERT INTO taikhoan (TenDN, MatKhau, NgayTao) VALUES (?, ?, NOW())";
+        $sql_taikhoan = "INSERT INTO taikhoan ( TenDN, MatKhau, NgayTao) VALUES ( ?, ?, NOW())";
         $stmt_taikhoan = $this->conn->prepare($sql_taikhoan);
         $stmt_taikhoan->bind_param('ss', $tendn, $matkhau);
         $stmt_taikhoan->execute();
